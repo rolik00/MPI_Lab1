ed2c34fe9d92ff2b5956f47d9bf645675ab129a3
rolik00 <lelya_rodina_01@mail.ru>
2025-11-06 22:47:21 +0300

    Добавьте файлы проекта.

---
+++ Project1/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <mpi.h>
+#include "firstTask.h"
+#include "fourthTask.h"
+
+int main() {
+
+	int my_rank;
+	int comm_sz;
+
+	MPI_Init(NULL, NULL);
+	MPI_Comm_size(MPI_COMM_WORLD, &comm_sz);
+	MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+
+	// Первое задание
+	first_task_execute(my_rank, comm_sz);
+
+	// Четвертое задание
+	fourth_task_execute(my_rank, comm_sz); // для демонстрации работы
+	fourth_task_report(my_rank, comm_sz); // для анализа работы алгоритма
+
+	MPI_Finalize();
+
+	return 0;
+}
